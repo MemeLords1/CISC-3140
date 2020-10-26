e94d569811bca46a8b3ab29397c2798d6587143f
Thomas Chen <42944780+MemeLords1@users.noreply.github.com>
2020-10-25 20:06:37 -0400

    Create Palindrome.scm

---
+++ Lab #8/Palindrome.scm
@@ -0,0 +1,17 @@
+;Task 1
+
+; use: (word " (the string input
+
+; returns: #t if true, #f is false
+(display "Enter using (word \" " )
+(define (word str)
+; store string in a list
+  (let loop ((str (string->list str))
+; reverse string in list form
+             (revstr (reverse (string->list str))))
+; check if string is empty
+    (or (null? str)
+; compare lists for equivalency
+        (and (char=? (car str) (car revstr))
+             (loop (cdr str) (cdr revstr))))))
+
